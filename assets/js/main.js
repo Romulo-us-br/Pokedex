771b35054ae49778c0f64b91a62c836374286e10
Romulus <romulop.brasil25@gmail.com>
2025-08-26 10:24:25 -0300

    PokedexInicio

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+  const btn1 = document.getElementById("btnTeste");
+        console.log(btn1);
+        btn1.addEventListener("click", function(){
+            alert("Botão apertado");
+        });
